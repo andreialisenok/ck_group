5d0a83842fbddeb72fe60aee75098e2d50f3fc35
Andrei Alisenok <andreialisenok@gmail.com>
2024-08-23 12:18:12 +0300

    fix project

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,41 @@
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+const application = document.querySelectorAll('.application');
+const body = document.querySelector('body');
+const modal = document.querySelector('.modal');
+const modalClose = document.querySelector('.modal__close');
+const inputsPhone = document.querySelectorAll("[type='tel']");
+application.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    modal.classList.toggle('active');
+    body.classList.toggle('isLock');
+  });
+});
+modalClose.addEventListener('click', () => {
+  modal.classList.toggle('active');
+  body.classList.toggle('isLock');
+});
+modalClose.addEventListener('click', () => {
+  menu.classList.toggle('isOpen');
+  burger.classList.toggle('isOpen');
+});
+inputsPhone.forEach((item) => {
+  window.intlTelInput(item, {
+    utilsScript:
+      'https://cdn.jsdelivr.net/npm/intl-tel-input@24.1.1/build/js/utils.js',
+    initialCountry: 'ru',
+    useFullscreenPopup: false,
+    separateDialCode: true,
+  });
+});
+
+burger.addEventListener('click', () => {
+  menu.classList.toggle('isOpen');
+  burger.classList.toggle('isOpen');
+});
+
 const tabBtns = document.querySelectorAll('.services .tabs button');
 const tabContent = document.querySelectorAll('.services .tabs-content');
-
-// tabBtns.forEach(function (el, index) {
-//   el.addEventListener('click', openTabs);
-// });
-
-// function openTabs(el, index) {
-//   var btnTarget = el.pointerId;
-//   console.log(index);
-//    var country = btnTarget.dataset.country;
-
-//    tabContent.forEach(function(el) {
-//       el.classList.remove("active");
-//    });
-
-//    tabLinks.forEach(function(el) {
-//       el.classList.remove("active");
-//    });
-
-//    document.querySelector("#" + country).classList.add("active");
-
-//    btnTarget.classList.add("active");
-// }
 
 tabBtns.forEach((tab, tabIndex) => {
   tab.addEventListener('click', () => {
@@ -39,4 +53,7 @@ tabBtns.forEach((tab, tabIndex) => {
       }
     });
   });
+});
+AOS.init({
+  duration: 1200,
 });
