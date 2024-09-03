5515dcd3a76c2bdb26e6d337143dfab4f4314928
Andrei Alisenok <andreialisenok@mdao.team>
2024-09-03 20:24:46 +0300

    fix pages

--- src/js/about.js
+++ src/js/about.js
@@ -68,133 +68,6 @@ AOS.init({
   duration: 1200,
 });
 
-// calc
-function setEquipment() {
-  const equipment =
-    document.getElementById('equipment').options[
-      document.getElementById('equipment').selectedIndex
-    ];
-  const consumption = equipment.getAttribute('data-consumption');
-  const hashrate = equipment.getAttribute('data-hashrate');
-  console.log(consumption, hashrate);
-  document.getElementById('consumption').value = consumption;
-  document.getElementById('hashrate').value = hashrate;
-}
-
-function updateResultTable(data) {
-  document.getElementById(
-    'dayIncome'
-  ).innerHTML = `${data.income.day} ${data.currency.name}`;
-  document.getElementById(
-    'weekIncome'
-  ).innerHTML = `${data.income.week} ${data.currency.name}`;
-  document.getElementById(
-    'monthIncome'
-  ).innerHTML = `${data.income.month} ${data.currency.name}`;
-  document.getElementById(
-    'dayOutGo'
-  ).innerHTML = `${data.outgo.day} ${data.currency.name}`;
-  document.getElementById(
-    'weekOutGo'
-  ).innerHTML = `${data.outgo.week} ${data.currency.name}`;
-  document.getElementById(
-    'monthOutGo'
-  ).innerHTML = `${data.outgo.month} ${data.currency.name}`;
-  document.getElementById(
-    'monthProfit'
-  ).innerHTML = `Прибыль в месяц: ${data.monthProfit} ${data.currency.name}`;
-}
-
-function calcResult() {
-  // const currencyRate = parseFloat(
-  //   document
-  //     .getElementById('currency')
-  //     .options[document.getElementById('currency').selectedIndex].getAttribute(
-  //       'data-rateUSD'
-  //     )
-  // );
-
-  const currencyRate = document
-    .querySelector('.currency__btn.active')
-    .getAttribute('data-rateUSD');
-  // const currency =
-  //   document.getElementById('currency').options[
-  //     document.getElementById('currency').selectedIndex
-  //   ].text;
-  const currency = document
-    .querySelector('.currency__btn.active')
-    .getAttribute('data-sign');
-  const commission = parseFloat(document.getElementById('commission').value);
-
-  const dayIncomeWithoutComission = calcIncome() * currencyRate;
-  const dayIncome =
-    Math.round(
-      (dayIncomeWithoutComission +
-        (dayIncomeWithoutComission * commission) / 100) *
-        100
-    ) / 100;
-  const weekIncome = Math.round(dayIncome * 7 * 100) / 100;
-  const monthIncome = Math.round(dayIncome * 30 * 100) / 100;
-
-  const dayOutGo = Math.round(calcOutGo() * currencyRate * 100) / 100;
-  const weekOutGo = Math.round(dayOutGo * 7 * 100) / 100;
-  const monthOutGo = Math.round(dayOutGo * 30 * 100) / 100;
-
-  const monthProfit = Math.round((monthIncome - monthOutGo) * 100) / 100;
-  updateResultTable({
-    currency: {
-      name: currency,
-    },
-    income: {
-      day: dayIncome,
-      week: weekIncome,
-      month: monthIncome,
-    },
-    outgo: {
-      day: dayOutGo,
-      week: weekOutGo,
-      month: monthOutGo,
-    },
-    monthProfit: monthProfit,
-  });
-}
-
-function calcOutGo() {
-  const consumption =
-    parseFloat(document.getElementById('consumption').value) / 1000;
-  const electricityCost = parseFloat(
-    document.getElementById('electricityCost').value
-  );
-  return consumption * electricityCost;
-}
-
-function calcIncome() {
-  const dailyIncomeTH_s_usdt = calcDailyIncomeTH_s_usdt();
-  const hashrate = parseFloat(document.getElementById('hashrate').value);
-  const result = dailyIncomeTH_s_usdt * hashrate;
-  return result;
-}
-
-function calcDailyIncomeTH_s_usdt() {
-  const dayInSec = 86400;
-  const difficulty = parseFloat(document.getElementById('difficulty').value);
-  const blockReward = parseFloat(document.getElementById('blockReward').value);
-  const hashrate_s = parseFloat(document.getElementById('hashrate_s').value);
-  const rateBTC = parseFloat(document.getElementById('rateBTC').value);
-
-  //console.log(dayInSec, blockReward, hashrate_s, difficulty);
-  const first = BigInt(dayInSec * blockReward * hashrate_s);
-  //console.log(first)
-  const second = BigInt(difficulty * 2 ** 32);
-  //console.log(second)
-  const incomeInCoins = (Number(first) / Number(second)).toFixed(8);
-  console.log(incomeInCoins);
-  //console.log(rateBTC)
-  const dailyIncomeTh_s_usdt = Number(incomeInCoins * rateBTC);
-  //console.log(dailyIncomeTh_s_usdt)
-  return dailyIncomeTh_s_usdt;
-}
-
 const data = [
   {
     year: 2014,
@@ -267,7 +140,6 @@ window.addEventListener('scroll', function (e) {
 
   if (currentData) {
     const index = data.indexOf(currentData);
-    console.log(index);
 
     $(numsBtns).removeClass('active');
     $(numsBtns).eq(index).addClass('active');
@@ -290,7 +162,6 @@ function scrollByPercent(percent) {
   console.log(percent);
   const percent2 = percent * 100;
   const totalHeight = document.documentElement.scrollHeight;
-  console.log(totalHeight);
   const targetPosition = totalHeight * (percent2 / 100);
   console.log(targetPosition);
 
@@ -310,23 +181,6 @@ $(numsBtns).on('click', function () {
   $(this).addClass('active');
 });
 
-// $(numsBtns).on('click', function () {
-//   $(numsBtns).removeClass('active');
-//   $(this).addClass('active');
-//   $('.line-wrap .about-page-second__row h5').text(this.textContent);
-//   // $('.line-wrap .about-page-second__row p').text(this.textContent);
-//   $(data).each(function (idx, value) {
-//     console.log(numsBtns);
-//     // console.log(value.text);
-//     // if (this.textContent === value.year) {
-//     //   console.log(value.text);
-//     // }
-//   });
-
-//   // $(data).each(function (idx, value) {
-//   //   console.log(value.text);
-//   // });
-// });
 const yearsBtns = document.querySelectorAll('.years__btn');
 const yearsTitle = document.querySelector('.about-page-second__row h5');
 yearsBtns.forEach((item, idx) => {
